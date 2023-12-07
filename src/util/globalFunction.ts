812fba7a131049f3a791d61677c806a1358ee137
Fanomezana Sarobidy RAKOTOMAHEFA <hei.sarobidy@gmail.com>
2023-12-07 14:54:49 +0300

    feat: global function

--- src/util/globalFunction.ts
+++ src/util/globalFunction.ts
@@ -1,4 +1,6 @@
-const parseTimeStamp = (timestamp: number) => {
+import { ActionSatusType, ActionType, HistoryType } from "./typeUtil";
+
+export const parseTimeStamp = (timestamp: number) => {
   const currentDate = new Date(timestamp);
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
@@ -6,9 +8,34 @@ const parseTimeStamp = (timestamp: number) => {
     day: "numeric",
     hour: "numeric",
     minute: "numeric",
+    second: "numeric",
   };
   const localeDateString = currentDate.toLocaleString("fr-FR", options);
   return localeDateString;
 };
+export const validateAmount = (amount: number, balance?: number) => {
+  let tempAmount = Number(amount);
+  if (!Boolean(tempAmount)) {
+    console.log("Enter a correct amount value.");
+  }
+  if (balance !== null && balance < amount) {
+    console.log("\nInsufficient balance.\n");
+  }
 
-export { parseTimeStamp };
+  return Boolean(tempAmount) && balance !== null && !(balance < amount);
+};
+
+export const newHistory = (
+  action: ActionType,
+  status: ActionSatusType,
+  amount?: number
+): HistoryType => {
+  const TIMESTAMP = Date.now();
+  return {
+    id: TIMESTAMP.toString(),
+    creationDatetime: parseTimeStamp(TIMESTAMP),
+    action,
+    status,
+    amount,
+  };
+};
